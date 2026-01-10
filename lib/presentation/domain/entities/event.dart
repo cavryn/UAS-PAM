321aa8d9808f279b87c94b472b71fc5635c13c7b
unknown <sheva7526@gmail.com>
2026-01-10 20:05:26 +0700

    entities

---
+++ lib/presentation/domain/entities/event.dart
@@ -0,0 +1,13 @@
+class Event {
+  final String id;
+  final String name;
+  final String description;
+  final String date;
+
+  Event({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.date,
+  });
+}

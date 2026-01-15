6646c82f75444c6a4bbbf5443806d1f572ba36c7
unknown <sheva7526@gmail.com>
2026-01-15 16:43:39 +0700

    add event model

---
+++ lib/presentation/data/models/event_model.dart
@@ -0,0 +1,19 @@
+import '../../domain/entities/event.dart';
+
+class EventModel extends Event {
+  EventModel({
+    required super.id,
+    required super.name,
+    required super.description,
+    required super.date,
+  });
+
+  factory EventModel.fromFirestore(Map<String, dynamic> json, String id) {
+    return EventModel(
+      id: id,
+      name: json['name'],
+      description: json['description'],
+      date: json['date'],
+    );
+  }
+}
